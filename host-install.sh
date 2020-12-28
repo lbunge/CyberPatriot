767cb1dd2a2869b2b5c680c5077aeb2d1ae0656e
lbunge <lukebunge@live.com>
2020-12-28 17:23:28 -0500

    forgot a p and installing firefox

--- host-install.sh
+++ host-install.sh
@@ -9,7 +9,7 @@ vncPass=${userPass:0:8}
 
 # Updates repos and installs dependencies & tightvncserver
 apt update; apt update; apt update  # Need to update repos multiple times
-apt install -y xfce4 xfce4-goodies wget gcc build-essential linux-headers-generic linux-headers-$(uname -r)
+apt install -y xfce4 xfce4-goodies wget gcc build-essential linux-headers-generic linux-headers-$(uname -r) firefox
 apt install -y tightvncserver
 wget --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0" https://www.vmware.com/go/getplayer-linux
 
@@ -20,7 +20,7 @@ chmod +x getplayer-linux
 # Get the Images from URLs
 wget $url1 -P /home/$newUser/Desktop/
 wget $url2 -P /home/$newUser/Desktop/
-wget $url3 - /home/$newUser/Desktop/
+wget $url3 -P /home/$newUser/Desktop/
 
 # Create the password & config file for the vnc service
 [[ ! -d /home/$newUser/.vnc ]] && mkdir /home/$newUser/.vnc
