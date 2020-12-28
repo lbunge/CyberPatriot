276f0aa1d02cd0262b9ba0a426d17366175d6bb8
lbunge <lukebunge@live.com>
2020-12-28 10:11:59 -0500

    added vnc password stuff

--- host-install.sh
+++ host-install.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 url1=$1
 url2=$2
 url3=$3
 newUser=$4
 userPass=$5
-
+vncPass=${userPass:0:8}
 echo "Url 1 is: $url1" >> /tmp/scriptOutput.txt
 echo "Url 1 is: $url2" >> /tmp/scriptOutput.txt
 echo "Url 1 is: $url3" >> /tmp/scriptOutput.txt
@@ -12,6 +14,12 @@ echo "User Password is: $userPass" >> /tmp/scriptOutput.txt
 
 ### Modifies the host vm's
 
-# apt update; apt -y upgrade
-# apt-get install -y xfce4 xfce4-goodies
-# apt-get install -y tightvncserver
+apt update; apt -y upgrade
+apt-get install -y xfce4 xfce4-goodies
+apt-get install -y tightvncserver
+
+# Create the password for the vnc service
+mkdir /home/$newUser/.vnc
+echo $vncPass | vncpasswd -f > /home/$newUser/.vnc/passwd
+chown -R $newUser:$newUser /home/$newUser/.vnc
+chmod 0600 /home/$newUser/.vnc/passwd
