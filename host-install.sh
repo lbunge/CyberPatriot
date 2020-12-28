b0016dcabe37276aaf9bd2fc353d6bcdbf1869ca
lbunge <lukebunge@live.com>
2020-12-28 11:19:27 -0500

    added vncserver config file

--- host-install.sh
+++ host-install.sh
@@ -15,12 +15,21 @@ echo "VNC Password is: $vncPass" >> /tmp/scriptOutput.txt
 
 ### Modifies the host vm's
 
-apt update; apt -y upgrade
+apt update
 apt install -y xfce4 xfce4-goodies
 apt install -y tightvncserver
 
-# Create the password for the vnc service
+# Create the password & config file for the vnc service
 mkdir /home/$newUser/.vnc
 echo $vncPass | vncpasswd -f > /home/$newUser/.vnc/passwd
+echo "#!/bin/bash" > /home/$newUser/.vnc/xstartup
+echo "xrdb $HOME/.Xresources" >> /home/$newUser/.vnc/xstartup
+echo "startxfce4 &" >> /home/$newUser/.vnc/xstartup
+
+# Ensure the proper permissions are set
 chown -R $newUser:$newUser /home/$newUser/.vnc
 chmod 0600 /home/$newUser/.vnc/passwd
+chmod +x /home/$newUser/.vnc/xstartup
+
+# Start the VNC server
+vncserver
