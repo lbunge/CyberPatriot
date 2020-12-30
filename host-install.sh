86b646e8633913471e75722a2856fe7e8f1aa0d6
lbunge <lukebunge@live.com>
2020-12-30 11:43:26 -0500

    reverted vmware player to v15

--- host-install.sh
+++ host-install.sh
@@ -11,11 +11,11 @@ vncPass=${userPass:0:8}
 apt update; apt update; apt update  # Need to update repos multiple times
 apt install -y xfce4 xfce4-goodies wget gcc build-essential linux-headers-generic linux-headers-$(uname -r) firefox
 apt install -y tightvncserver
-wget --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0" https://www.vmware.com/go/getplayer-linux
+wget --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0" https://download3.vmware.com/software/player/file/VMware-Player-15.5.7-17171714.x86_64.bundle
 
-# Install Vmware Workstation
-chmod +x getplayer-linux
-./getplayer-linux
+# Install Vmware Workstation Player
+chmod +x VMware-Player-15.5.7-17171714.x86_64.bundle
+./VMware-Player-15.5.7-17171714.x86_64.bundle
 
 # Get the Images from URLs
 wget $url1 -P /home/$newUser/Desktop/
